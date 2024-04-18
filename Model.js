15bffacc9e1e0d924f56955946885f6d21787603
Nialoshaar <valerian.alamar@epitech.eu>
2024-04-18 21:59:46 +0200

    Added week variable to calculate the current week of the run and place accordingly in the sheet

--- Model.js
+++ Model.js
@@ -1,4 +1,4 @@
-const { tagmanager } = require("googleapis/build/src/apis/tagmanager");
+const utils = require('./utils.js');
 
 // Model.js
 class RunModel {
@@ -7,30 +7,34 @@ class RunModel {
     }
 
     setData(run, runDetails, characterName, id) {
+        const tank = runDetails.roster.find(character => character.role === 'tank');
+        const healer = runDetails.roster.find(character => character.role === 'healer');
+        const dps = runDetails.roster.filter(character => character.role === 'dps');
+        const upperCaseCharName = characterName.charAt(0).toUpperCase() + characterName.slice(1);
+        const mychar = runDetails.roster.find(character => character.character.name === upperCaseCharName);
+        const mycharSpec = mychar.character.spec ? mychar.character.spec.name : 'Unknown';
+
+
         const recentRun = {
-            characterName: characterName,
-            characterSpec: runDetails.roster.find(character => character.character.name.toLowerCase() === characterName.toLowerCase()).spec,
+            characterName: upperCaseCharName,
+            characterSpec: mycharSpec,
             runId: id,
             dungeon: run.short_name,
             level: run.mythic_level,
             affix1: run.affixes[0].name,
             affix2: run.affixes[1].name,
             affix3: run.affixes[2].name,
+            week: utils.assignRunToWeek(run.completed_at),
             date: run.completed_at,
             numChests: runDetails.num_chests,
-            player1: runDetails.roster[0].character,
-            player2: runDetails.roster[1].character,
-            player3: runDetails.roster[2].character,
-            player4: runDetails.roster[3].character,
-            //player5: runDetails.roster[4].character,
+            tank: tank ? tank.character.spec.name : 'Unknown',
+            healer: healer ? healer.character.spec.name : 'Unknown',
+            dps1: dps.length > 0 ? dps[0].character.spec.name : 'Unknown',
+            dps2: dps.length > 1 ? dps[1].character.spec.name : 'Unknown',
+            dps3: dps.length > 2 ? dps[2].character.spec.name : 'Unknown',
         };
         // Append the recent run to the data array
         this.data.push(recentRun);
-    }
-
-    extractRunId(url) {
-        const parts = url.split('/');
-        return parts[parts.length - 1].split('-')[0];
     }
 
     getData() {
